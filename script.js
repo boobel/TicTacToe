f191dd6322afe1be6c12b094be6dd743e543b867
boobelgit config --global user.email patryk1borowski@gmail.com <patryk1borowski@gmail.com>
2022-07-15 01:10:01 +0200

    implementation of win/lose logic, bit of work on displaying the result

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const fieldElements = document.querySelectorAll(".field");
 
 
-const playerFactory = () => {
+const playerFactory = (mark) => {
+    this.mark = mark;
 
+  const getMark = () => {
+    return mark;
+  };
+
+  return { getMark };
 };
 
 const gameBoard = (() => {
@@ -21,13 +27,42 @@ const gameBoard = (() => {
     return {board, reset};
 })();
 
+const gameController = (() => { 
+
+    const checkWinner = (mark) => {
+        return ((gameBoard.board[0]=== mark)&&((gameBoard.board[0]===gameBoard.board[1])&&(gameBoard.board[0]===gameBoard.board[2]))||
+            (gameBoard.board[3]=== mark)&&((gameBoard.board[3]===gameBoard.board[4])&&(gameBoard.board[3]===gameBoard.board[5]))||
+            (gameBoard.board[6]=== mark)&&((gameBoard.board[6]===gameBoard.board[7])&&(gameBoard.board[6]===gameBoard.board[8]))||
+            (gameBoard.board[0]=== mark)&&((gameBoard.board[0]===gameBoard.board[3])&&(gameBoard.board[0]===gameBoard.board[6]))||
+            (gameBoard.board[1]=== mark)&&((gameBoard.board[1]===gameBoard.board[4])&&(gameBoard.board[1]===gameBoard.board[7]))||
+            (gameBoard.board[2]=== mark)&&((gameBoard.board[2]===gameBoard.board[5])&&(gameBoard.board[2]===gameBoard.board[8]))||
+            (gameBoard.board[0]=== mark)&&((gameBoard.board[0]===gameBoard.board[4])&&(gameBoard.board[0]===gameBoard.board[8]))||
+            (gameBoard.board[2]=== mark)&&((gameBoard.board[0]===gameBoard.board[4])&&(gameBoard.board[0]===gameBoard.board[6])));    
+    };
+
+    const checkDraw = () => {
+        var drawIndex = 0;
+        fieldElements.forEach((field)=> {
+            if(field.innerHTML !== '') {
+                drawIndex +=1;
+            }
+        });
+        return (drawIndex === 9);
+    };
+    return {checkWinner, checkDraw};
+})();
+
 const displayController = (() => {
     var turnIndex = 0;
-    var turn  = document.querySelector("#turn")
-    const resetBtn = document.querySelector('#restart-button')
-    turn.innerHTML = 'X'
+    var turn  = document.querySelector("#turn");
+    const resetBtn = document.querySelector('#restart-button');
+    const result = document.querySelector('#result');
+    turn.innerHTML = 'X';
+    let isWinnerO, isWinnerX = false;
 
     resetBtn.addEventListener('click', gameBoard.reset)
+
+    
 
     fieldElements.forEach((field) => {
         field.addEventListener('click', () => {
@@ -36,41 +71,40 @@ const displayController = (() => {
                 gameBoard.board[field.dataset.index] = field.innerHTML;
                 turnIndex = 1;
                 turn.innerHTML = 'O';
+                if(gameController.checkWinner('X') === true) {
+                    console.log('X wins');
+                    isWinnerX = true;
+                }
+                if(gameController.checkDraw() === true) {
+                    console.log('Draw');
+                }
             }
             else if (field.innerHTML === '' && turnIndex === 1){
                 field.innerHTML = 'O';
                 gameBoard.board[field.dataset.index] = field.innerHTML;
                 turnIndex = 0;
                 turn.innerHTML = 'X';
+                if(gameController.checkWinner('O') === true) {
+                    console.log('O wins');
+                    isWinnerO = true;
+                }
+                if(gameController.checkDraw() === true) {
+                    console.log('Draw');
+                }
             };
         });
     });
 
-    return {fieldElements}
-})();
-
-
-const gameController = (() => {
-
-    const checkWinner = () => {
-
-        
-        if () {
-            return ((gameBoard.board[0] === gameBoard.board[1] === gameBoard.board[2]) ||
-                (gameBoard.board[3] === gameBoard.board[4] === gameBoard.board[5]) ||
-                (gameBoard.board[6] === gameBoard.board[7] === gameBoard.board[8]) ||
-                (gameBoard.board[0] === gameBoard.board[3] === gameBoard.board[6]) ||
-                (gameBoard.board[1] === gameBoard.board[4] === gameBoard.board[7]) ||
-                (gameBoard.board[2] === gameBoard.board[5] === gameBoard.board[7]) ||
-                (gameBoard.board[3] === gameBoard.board[6] === gameBoard.board[8]) ||
-                (gameBoard.board[0] === gameBoard.board[4] === gameBoard.board[8]) ||
-                (gameBoard.board[2] === gameBoard.board[4] === gameBoard.board[6])
-            );
+    const displayResult = (()=> {
+        if(isWinnerX === true) {
+            result.innerHTML = 'Player X won!'
         }
-    };
-    return {checkWinner};
+        else if(isWinnerO === true) {
+            result.innerHTML = 'Player O won!'
+        }
+    })();
+
+    return {fieldElements, isWinnerO, isWinnerX, displayResult}
 })();
-
-
 
 
